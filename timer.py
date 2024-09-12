7c1b57a53b2f8a64c777cd9d865eb53d5ef6c999
nahin0x <rajibami23@gmail.com>
2024-09-12 18:00:03 +0600

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,12 @@
+import time
+
+inp=int(input("Enter the time in seconds: "))
+# print(" ")
+for sc in range(inp,0,-1):
+    seconds=sc%60
+    minites=int(sc/60)%60
+    hours=int(sc/3600)
+    print(f"{hours:02}:{minites:02}:{seconds:02}")
+    time.sleep(1)
+    
+print("Time's Up !!!")
